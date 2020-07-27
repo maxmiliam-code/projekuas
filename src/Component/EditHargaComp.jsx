1309dea7f5bdbb11e21a22eef4b845dc6b39301c
Michael Oki <oki.mikael@outlook.com>
2020-07-27 13:32:28 +0700

    Berhasil Menampilkan dan Mengedit Harga

--- src/Component/EditHargaComp.jsx
+++ src/Component/EditHargaComp.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
+import qs from 'querystring'
+import {Container, Col, Row, Form, FormGroup, Label, Input, Button, Alert} from 'reactstrap'
+//import {Link} from 'react-router-dom'
 
-const EditHargaComp = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+ const api = "http://localhost:3002"
+
+class EditHargaComp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id_harga : this.props.location.state.id_harga,
+            harga : this.props.location.state.harga,
+            response:'',
+            display:'none'
+        }
+    }
+
+    handleChange= (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    ubahharga = (idharga) =>{
+        const data = qs.stringify({
+            id_harga: idharga,
+            harga: this.state.harga
+        })
+
+        axios.put(api+ '/ubah/harga', data)
+        .then(json => {
+            if(json === 200){
+                this.setState({
+                    response: json.data.values,
+                    display: 'block'
+                })
+            }else {
+                this.setState({
+                    response: json.data.values,
+                    display: 'block'
+                })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <h4>Form Edit Data</h4>
+                <Alert color="success" style={{display: this.state.display}}>
+                    {this.state.response}
+                </Alert>
+                <Form className="form">
+                    <Col>
+                    <Label>Harga</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="text" name="harga" value={this.state.harga} onChange={this.handleChange} placeholder="Masukan Nama"/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Button type="button" onClick={()=>this.ubahharga(this.state.id_harga)}>Submit</Button>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    </Col>
+                </Form>
+            </Container>
+        );
+    }
+}
 
 export default EditHargaComp;
--- src/Component/HargaComp.jsx
+++ src/Component/HargaComp.jsx
@@ -1,11 +1,68 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
+import {Table, Button, Container, Alert} from 'reactstrap'
+import {Link} from 'react-router-dom'
 
-const HargaComp = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const api = 'http://localhost:3002'
 
-export default HargaComp;
+class HargaComp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            harga: [],
+            response: '',
+            display: 'none'
+        }
+    }
+
+    componentDidMount(){
+        axios.get(api+'/tampil/harga').then(res=>{
+            this.setState({
+                harga: res.data.values
+            })
+        })
+    }
+    
+    render() {
+        return (
+            <Container>
+                <h2>Harga</h2>
+                <Alert color="success" style={{display: this.state.display}}>
+                    {this.state.response}
+                </Alert>
+                <Table className="table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Harga</th>
+                            <th>Opsi</th>
+                        </tr>                        
+                    </thead>
+                    <tbody>
+                        {this.state.harga.map(harga =>
+                            <tr key={harga.id_harga}>
+                                <td>{harga.harga}</td>
+                                <td>
+                                    <Link to = {
+                                        {
+                                            pathname: '/harga/edit',
+                                            state: {
+                                                id_harga: harga.id_harga,
+                                                harga: harga.harga,
+                                            }
+                                        }
+                                    }>
+                                        <Button>Edit</Button> 
+                                    </Link>
+                                </td>
+                            </tr>
+
+                            )}
+                    </tbody>
+                </Table>
+            </Container>
+        )
+    }
+}
+
+export default HargaComp
