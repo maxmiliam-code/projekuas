06a97dc97b22776254962699d303ceffab1a2c40
Michael Oki <oki.mikael@outlook.com>
2020-07-27 13:34:28 +0700

    Berhasil membuat ganti password

--- src/Component/EditUser.jsx
+++ src/Component/EditUser.jsx
@@ -1,11 +1,127 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
+import qs from 'querystring'
+import {Container, Col, Row, Form, FormGroup, Label, Input, Button, Alert} from 'reactstrap'
+//import {Link} from 'react-router-dom'
 
-const EditUser = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+ const api = "http://localhost:3002"
+
+class EditUser extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            id : this.props.location.state.id,
+            username : this.props.location.state.username,
+            email : this.props.location.state.email,
+            password : this.props.location.state.password,
+            role : this.props.location.state.role,
+            tanggal_daftar : this.props.location.state.tanggal_daftar,
+            response:'',
+            display:'none'
+        }
+    }
+
+    handleChange= (e) => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+    ubahUser = (iduser) =>{
+        const data = qs.stringify({
+            id: iduser,
+            username: this.state.username,
+            email: this.state.email,
+            password: this.state.password,
+            role: this.state.role,
+            tanggal_daftar: this.state.tanggal_daftar
+        })
+
+        axios.put(api+ '/ubah/user', data)
+        .then(json => {
+            if(json === 200){
+                this.setState({
+                    response: json.data.values,
+                    display: 'block'
+                })
+            }else {
+                this.setState({
+                    response: json.data.values,
+                    display: 'block'
+                })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <Container>
+                <h4>Edit Data User </h4>
+                <Alert color="success" style={{display: this.state.display}}>
+                    {this.state.response}
+                </Alert>
+                <Form className="form">
+                    <Col>
+                    <Label>No</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="text" name="id" value={this.state.id} onChange={this.handleChange} placeholder="Masukan Id"/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <Label>Username</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="text" name="username" value={this.state.username} onChange={this.handleChange} placeholder="Masukan Username"/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <Label>Email</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="text" name="email" value={this.state.email} onChange={this.handleChange} placeholder="Masukan Email"/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <Label>Password </Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="text" name="password" value={this.state.password} onChange={this.handleChange} placeholder="Masukan Password "/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <Label>Role</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="text" name="role" value={this.state.role} onChange={this.handleChange} placeholder="Masukan Role "/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <Label>Tanggal Daftar</Label>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Input type="date" name="tanggal_daftar" value={this.state.tanggal_daftar} onChange={this.handleChange} placeholder="Masukan Tanggal Daftar"/>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+                    <FormGroup>
+                        <Row>
+                            <Col>
+                                <Button type="button" onClick={()=>this.ubahUser(this.state.id)}>Submit</Button>
+                            </Col>
+                        </Row>
+                    </FormGroup>
+
+                    </Col>
+                </Form>
+            </Container>
+        );
+    }
+}
 
 export default EditUser;
--- src/Component/UserComp.jsx
+++ src/Component/UserComp.jsx
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react'
+import axios from 'axios'
+import {Table, Button, Container, Alert} from 'reactstrap'
+import {Link} from 'react-router-dom'
 
-const UserComp = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const api = 'http://localhost:3002'
 
-export default UserComp;
+class UserComp extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            user: [],
+            response: '',
+            display: 'none'
+        }
+    }
+
+    componentDidMount(){
+        axios.get(api+'/tampil/user').then(res=>{
+            this.setState({
+                user: res.data.values
+            })
+        })
+    }
+    
+    render() {
+        return (
+            <Container>
+                <h2>user</h2>
+                <Alert color="success" style={{display: this.state.display}}>
+                    {this.state.response}
+                </Alert>
+                <Table className="table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Username</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                            <th>role</th>
+                            <th>Tanggal Daftar</th>
+                            <th>Opsi</th>
+                        </tr>                        
+                    </thead>
+                    <tbody>
+                        {this.state.user.map(user =>
+                            <tr key={user.id}>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>{user.password}</td>
+                                <td>{user.role}</td>
+                                <td>{user.tanggal_daftar}</td>
+
+                                <td>
+                                    <Link to = {
+                                        {
+                                            pathname: '/user/edit',
+                                            state: {
+                                                id: user.id,
+                                                user: user.user,
+                                            }
+                                        }
+                                    }>
+                                        <Button>Edit</Button> 
+                                    </Link>
+                                </td>
+                            </tr>
+
+                            )}
+                    </tbody>
+                </Table>
+            </Container>
+        )
+    }
+}
+
+export default UserComp
